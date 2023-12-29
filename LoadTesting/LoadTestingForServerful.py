de07c40b3664703431787832e51785a77a6e3c77
vvvi <wangyxin517@163.com>
2023-12-29 22:12:01 +0800

    fix：重要更新！修改压测逻辑，否则请求数有很大问题

--- LoadTesting/LoadTestingForServerful.py
+++ LoadTesting/LoadTestingForServerful.py
@@ -2,8 +2,9 @@ import datetime
 import pandas as pd
 import math
 import random
+import time
 
-from locust import LoadTestShape, task, constant, HttpUser
+from locust import LoadTestShape, task,  constant, HttpUser
 
 data_path="D:/CodingWork/git/Distributed-Inference-of-Ensemble-Learning-Models/testDataSets/stockPredict/dji_x_test_full.csv"
 df = pd.read_csv(data_path, header=None)
@@ -11,27 +12,35 @@ processed_data = df.apply(lambda row: ','.join(map(str, row)), axis=1)
 datas=processed_data.tolist()
 datas_length = len(datas)
 
-invoke_data_path="invoke_data.csv"
+invoke_data_path="invoke_data2.csv"
 invoke_data = pd.read_csv(invoke_data_path, header=None)
 invoke_data_length = len(invoke_data)
 
 #所有的时间单位都是秒
-pressure_test_interval=3   #每两个并发的间隔时间
-single_test_duration=60    #一批次压测时间
-single_stop_duration=30    #一批次停止压测时间
+pressure_test_interval=6   #每两个并发的间隔时间
+single_test_duration=150   #一批次压测时间
+single_stop_duration=15    #一批次停止压测时间
 max_request_time=20        #预估一个请求的最大时间,用于限制压测的执行时长
 
 class MyUser(HttpUser):
-    wait_time = constant(1)
-    host = "http://10.60.150.177:32057"  # 设置主机地址
+    host = "http://10.60.150.177:32056"  # 设置主机地址
 
     @task(1)
     def task_1(self):
+        start_time = time.time()
+
         global datas_length,datas
         random_number = random.randint(0, datas_length-1)
         data=datas[random_number]
         response = self.client.post('/',data=data)
         print(f"Response status code: {response.status_code}")
+        time_delay = time.time() - start_time
+
+        if time_delay > pressure_test_interval:
+            pass
+        else:
+            wait_time = pressure_test_interval - time_delay
+            time.sleep(wait_time)
 
 class MyCustomShape(LoadTestShape):
     '''
@@ -48,7 +57,7 @@ class MyCustomShape(LoadTestShape):
         is_in_test_duration = (run_time % (single_test_duration+single_stop_duration)) < single_test_duration
         
         if run_time < self.time_limit and is_in_test_duration:
-            index = int((int(run_time/pressure_test_interval)+1) % invoke_data_length)
+            index = (int(run_time/pressure_test_interval)+1)
 
             if index >= invoke_data_length: #如果超过了数据的长度,就停止压测
                 return None
